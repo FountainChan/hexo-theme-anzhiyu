d7196c767388185b05f31c1b8979557a96199959
Fountain <fountain.chan19@gmail.com>
2026-08-07 22:43:57 +0800

    feat: 分类页子分类切换 tabs，catalog_list 只显示顶级分类

--- scripts/helpers/catalog_list.js
+++ scripts/helpers/catalog_list.js
@@ -1,6 +1,9 @@
 hexo.extend.helper.register("catalog_list", function (type) {
   let html = ``;
-  hexo.locals.get(type).map(function (item) {
+  hexo.locals.get(type).filter(function (item) {
+    // 只显示顶级分类（parent 为空），子分类由分类页自己渲染
+    return !item.parent
+  }).map(function (item) {
     html += `
     <div class="catalog-list-item" id="/${item.path}">
       <a href="/${item.path}">
